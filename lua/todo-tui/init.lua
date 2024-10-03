35f8d26a7475663cab90829906fdc02f9e03d6b0
saltchicken <john.eicher89@gmail.com>
2024-10-02 18:15:30 -0700

    Remove erroneous stuff

--- lua/todo-tui/init.lua
+++ lua/todo-tui/init.lua
@@ -1,38 +1,5 @@
 local todo_tui = {}
 
-todo_tui.setup = function(opts)
-	-- require("nvim-ollama-pilot.request").setup(opts)
-	print("hello world")
-	local nui = require("nui")
-	local popup = require("nui.popup")
-
-	local win = popup({
-		enter = true,
-		focusable = true,
-		border = {
-			style = "single",
-			text = {
-				top = "Your TUI",
-				top_align = "center",
-			},
-		},
-		position = "50%",
-		size = {
-			width = 80,
-			height = 20,
-		},
-		buf_options = {
-			modifiable = true,
-			readonly = false,
-		},
-	})
-
-	win:mount()
-
-	-- Set up keymaps to close or handle inputs
-	win:on("BufLeave", function()
-		win:unmount()
-	end)
-end
+todo_tui.setup = function(opts) end
 
 return todo_tui
