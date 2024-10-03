f7ca60f0a890d8c2fd2bcff8a869bf1e46d347a1
saltchicken <john.eicher89@gmail.com>
2024-10-02 20:36:21 -0700

    Read file func

--- lua/todo-tui/init.lua
+++ lua/todo-tui/init.lua
@@ -23,44 +23,41 @@ function ShowMenu(opts, cb)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent = false })
 end
 
--- Function to write to a file
--- local function write_file(filepath, content)
--- 	local file = io.open(filepath, "w") -- open the file in write mode ("w" overwrites the file)
--- 	if not file then
--- 		vim.api.nvim_err_writeln("Could not open file: " .. filepath)
--- 		return false
--- 	end
--- 	file:write(content) -- write the content to the file
--- 	file:close() -- close the file
--- 	return true
--- end
-
--- Example usage in your plugin
+local function write_file(filepath, content)
+	local file = io.open(filepath, "w") -- open the file in write mode ("w" overwrites the file)
+	if not file then
+		vim.api.nvim_err_writeln("Could not open file: " .. filepath)
+		return false
+	end
+	file:write(content) -- write the content to the file
+	file:close() -- close the file
+	return true
+end
 
 function MyMenu()
-	-- local success = write_file("./temp", "hello there")
-	--
-	-- if success then
-	-- 	print("File written successfully!")
-	-- else
-	-- 	print("Failed to write the file.")
-	-- end
-	-- local file = io.open("~/todo", "r")
-	-- local file = { "hello", "there" }
-	-- local opts = {}
-	-- if file then
-	-- 	for line in file:lines() do
-	-- 		table.insert(opts, line)
-	-- 	end
-	-- else
-	-- 	opts = { "Need file" }
-	-- end
+	local success = write_file("./temp", "hello there")
+
+	if success then
+		print("File written successfully!")
+	else
+		print("Failed to write the file.")
+	end
+
+	local file = io.open("~/todo", "r")
+	local opts = {}
+	if file then
+		for line in file:lines() do
+			table.insert(opts, line)
+		end
+	else
+		opts = { "Need file" }
+	end
 	-- Hardcorded opts
-	local opts = {
-		"First line",
-		"Second line",
-		"Third line",
-	}
+	-- local opts = {
+	-- 	"First line",
+	-- 	"Second line",
+	-- 	"Third line",
+	-- }
 	local cb = function(_, sel)
 		-- vim.cmd("cd " .. sel)
 		vim.cmd("echo " .. sel)
