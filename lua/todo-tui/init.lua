43e7b9167842786270c7b180f6a30a701aeabb98
saltchicken <john.eicher89@gmail.com>
2024-10-02 19:40:21 -0700

    Add test

--- lua/todo-tui/init.lua
+++ lua/todo-tui/init.lua
@@ -1,5 +1,30 @@
 local todo_tui = {}
 
-todo_tui.setup = function(opts) end
+local popup = require("plenary.popup")
+
+local Win_id
+
+function ShowMenu(opts, cb)
+	local height = 20
+	local width = 30
+	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+	Win_id = popup.create(opts, {
+		title = "MyProjects",
+		highlight = "MyProjectWindow",
+		line = math.floor(((vim.o.lines - height) / 2) - 1),
+		col = math.floor((vim.o.columns - width) / 2),
+		minwidth = width,
+		minheight = height,
+		borderchars = borderchars,
+		callback = cb,
+	})
+	local bufnr = vim.api.nvim_win_get_buf(Win_id)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent = false })
+end
+
+todo_tui.setup = function(opts)
+	ShowMenu()
+end
 
 return todo_tui
