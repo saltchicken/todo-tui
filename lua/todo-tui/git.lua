b873969406f9ebe069805bcd846bb969e244edcb
saltchicken <john.eicher89@gmail.com>
2024-10-06 15:16:39 -0700

    Fix git so that repo path can be assigned from lazy plugin

--- lua/todo-tui/git.lua
+++ lua/todo-tui/git.lua
@@ -4,9 +4,73 @@ local Job = require("plenary.job")
 
 git.setup = function(opts)
 	git.repo_path = opts.repo_path
-end
 
--- git.repo_path = "/home/saltchicken/.local/share/keep"
+	git.add = Job:new({
+		command = "git",
+		args = { "add", "." },
+		cwd = git.repo_path, -- Set this to the appropriate repo path
+		on_start = function()
+			print("Running git add...")
+		end,
+		on_stdout = function(_, data)
+			print("stdout:", data)
+		end,
+		on_stderr = function(_, data)
+			print("stderr:", data)
+		end,
+		on_exit = function(_, return_val)
+			if return_val == 0 then
+				print("Git add successful!")
+			else
+				print("Git add failed!")
+			end
+		end,
+	})
+
+	git.commit = Job:new({
+		command = "git",
+		args = { "commit", "-m", "Update" }, --TODO: Change hardcorded commit message
+		cwd = git.repo_path, -- Set this to the appropriate repo path
+		on_start = function()
+			print("Running git commit...")
+		end,
+		on_stdout = function(_, data)
+			print("stdout:", data)
+		end,
+		on_stderr = function(_, data)
+			print("stderr:", data)
+		end,
+		on_exit = function(_, return_val)
+			if return_val == 0 then
+				print("Git commit successful!")
+			else
+				print("Git commit failed!")
+			end
+		end,
+	})
+
+	git.push = Job:new({
+		command = "git",
+		args = { "push" },
+		cwd = git.repo_path, -- Set this to the appropriate repo path
+		on_start = function()
+			print("Pushing to git...")
+		end,
+		on_stdout = function(_, data)
+			print("stdout:", data)
+		end,
+		on_stderr = function(_, data)
+			print("stderr:", data)
+		end,
+		on_exit = function(_, return_val)
+			if return_val == 0 then
+				print("Git push successful!")
+			else
+				print("Git push failed!")
+			end
+		end,
+	})
+end
 
 git.pull = function()
 	Job:new({
@@ -23,80 +87,14 @@ git.pull = function()
 	}):start()
 end
 
-local add = Job:new({
-	command = "git",
-	args = { "add", "." },
-	cwd = git.repo_path, -- Set this to the appropriate repo path
-	on_start = function()
-		print("Running git add...")
-	end,
-	on_stdout = function(_, data)
-		print("stdout:", data)
-	end,
-	on_stderr = function(_, data)
-		print("stderr:", data)
-	end,
-	on_exit = function(_, return_val)
-		if return_val == 0 then
-			print("Git add successful!")
-		else
-			print("Git add failed!")
-		end
-	end,
-})
-
-local commit = Job:new({
-	command = "git",
-	args = { "commit", "-m", "Update" }, --TODO: Change hardcorded commit message
-	cwd = git.repo_path, -- Set this to the appropriate repo path
-	on_start = function()
-		print("Running git commit...")
-	end,
-	on_stdout = function(_, data)
-		print("stdout:", data)
-	end,
-	on_stderr = function(_, data)
-		print("stderr:", data)
-	end,
-	on_exit = function(_, return_val)
-		if return_val == 0 then
-			print("Git commit successful!")
-		else
-			print("Git commit failed!")
-		end
-	end,
-})
-
-local push = Job:new({
-	command = "git",
-	args = { "push" },
-	cwd = git.repo_path, -- Set this to the appropriate repo path
-	on_start = function()
-		print("Pushing to git...")
-	end,
-	on_stdout = function(_, data)
-		print("stdout:", data)
-	end,
-	on_stderr = function(_, data)
-		print("stderr:", data)
-	end,
-	on_exit = function(_, return_val)
-		if return_val == 0 then
-			print("Git push successful!")
-		else
-			print("Git push failed!")
-		end
-	end,
-})
-
 git.add_commit_push = function()
-	commit:after(function()
-		push:start()
+	git.commit:after(function()
+		git.push:start()
 	end)
-	add:after(function()
-		commit:start()
+	git.add:after(function()
+		git.commit:start()
 	end)
-	add:start()
+	git.add:start()
 end
 
 return git
