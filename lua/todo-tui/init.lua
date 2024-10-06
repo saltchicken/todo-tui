6fce33d21d37864d5df921391523a730cdb28f5e
saltchicken <john.eicher89@gmail.com>
2024-10-06 15:57:02 -0700

    Refactor popup into its own file

--- lua/todo-tui/init.lua
+++ lua/todo-tui/init.lua
@@ -1,49 +1,14 @@
 local todo_tui = {}
 
-local popup = require("plenary.popup")
 local git = require("todo-tui.git")
 local file = require("todo-tui.file")
+local keep_popup = require("todo-tui.popup")
 
 local repo_path
 todo_tui.setup = function(opts)
 	git.setup(opts)
 	repo_path = opts.repo_path
 	require("todo-tui.keymaps").set_keymaps()
-end
-
-local Win_id
-
-function ShowMenu(opts, cb)
-	local screen_width = vim.o.columns
-	local screen_height = vim.o.lines
-	local popup_width = math.floor(screen_width * 0.9)
-	local popup_height = math.floor(screen_height * 0.4)
-	-- local popup_height = 40
-	-- local popup_width = 60
-	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-
-	Win_id = popup.create(opts.contents, {
-		title = opts.title,
-		highlight = "TODOHIGHLIGHT",
-		line = math.floor(((vim.o.lines - popup_height) / 2) - 1),
-		col = math.floor((vim.o.columns - popup_width) / 2),
-		minwidth = popup_width,
-		minheight = popup_height,
-		borderchars = borderchars,
-		callback = cb,
-		wrap = true,
-	})
-	local bufnr = vim.api.nvim_win_get_buf(Win_id)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent = false })
-
-	vim.api.nvim_create_autocmd({ "BufWipeout", "BufDelete" }, {
-		buffer = bufnr,
-		callback = function()
-			git.add_commit_push()
-		end,
-	})
-
-	git.pull()
 end
 
 local function write_current_to_file()
@@ -61,7 +26,7 @@ function KeepTodo()
 	local cb = function(_, sel)
 		write_current_to_file()
 	end
-	ShowMenu(opts, cb)
+	keep_popup.show_menu(opts, cb)
 end
 
 return todo_tui
