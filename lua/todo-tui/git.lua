d81b9def9c9a4b5b268ea570a1e2fa9757c1b023
saltchicken <john.eicher89@gmail.com>
2024-10-10 16:38:19 -0700

    Fix git functionality

--- lua/todo-tui/git.lua
+++ lua/todo-tui/git.lua
@@ -4,7 +4,7 @@ local Job = require("plenary.job")
 
 git.current_revision = nil
 
-git.setup = function(opts)
+git.setup = function(opts, Windows)
 	git.repo_path = opts.repo_path
 
 	git.add = Job:new({
@@ -72,20 +72,62 @@ git.setup = function(opts)
 			end
 		end,
 	})
-end
+	git.fetch = Job:new({
+		command = "git",
+		args = { "fetch" },
+		cwd = git.repo_path,
+		on_exit = function(_, return_val)
+			if return_val == 0 then
+				print("Fetch successful")
+			else
+				print("Failed to fetch")
+			end
+		end,
+	})
+	git.status = Job:new({
+		command = "git",
+		args = { "status", "--branch", "--porcelain" },
+		cwd = git.repo_path,
+		on_exit = function(job, return_val)
+			if return_val == 0 then
+				local result = table.concat(job:result(), "\n")
+				print(result)
+				if result:match("behind") then
+					vim.schedule_wrap(function()
+						Windows:yes_no_prompt("Update available", function()
+							git.pull:start()
+						end, nil)
+					end)()
+				else
+					print("Up to date")
+				end
+			else
+				print(table.concat(job:result(), "\n"))
+				print("Error running git status")
+			end
+		end,
+	})
 
-git.pull = Job:new({
-	command = "git",
-	args = { "pull" },
-	cwd = git.repo_path,
-	on_exit = function(j, return_val)
-		if return_val == 0 then
-			print("Git folder updated successfully!")
-		else
-			print("Failed to update git folder.")
-		end
-	end,
-})
+	git.check_update_available = function()
+		git.fetch:after(function()
+			git.status:start()
+		end)
+		git.fetch:start()
+	end
+
+	git.pull = Job:new({
+		command = "git",
+		args = { "pull" },
+		cwd = git.repo_path,
+		on_exit = function(j, return_val)
+			if return_val == 0 then
+				print("Git folder updated successfully!")
+			else
+				print("Failed to update git folder.")
+			end
+		end,
+	})
+end
 
 git.add_commit_push = function()
 	git.commit:after(function()
@@ -97,53 +139,53 @@ git.add_commit_push = function()
 	git.add:start()
 end
 
-git.get_current_revision = Job:new({
-	command = "git",
-	args = { "rev-parse", "HEAD" },
-	cwd = git.repo_path,
-	on_exit = function(job, return_val)
-		if return_val == 0 then
-			local commit_hash = table.concat(job:result(), "\n")
-			git.current_revision = commit_hash
-		else
-			print("Failed to get the current revision")
-		end
-	end,
-})
-
-git.compare_local_to_remote = Job:new({
-	command = "git",
-	args = { "ls-remote", "origin", "HEAD" },
-	cwd = git.repo_path,
-	on_exit = function(job, return_val)
-		if return_val == 0 then
-			local result = table.concat(job:result(), "\n")
-			print(result)
-			local remote_commit_hash = result:match("^(.*)\t") or result
-			print("Local: " .. git.current_revision)
-			print("Remote: " .. remote_commit_hash)
-			if remote_commit_hash == git.current_revision then
-				print("Up to date")
-			else
-				print("Update available")
-			end
-		else
-			print("Failed to get the remote revision")
-		end
-	end,
-})
-
-git.check_update_available = function()
-	-- TODO: Make this its own plenary job
-	local current_directory
-	vim.fn.chdir(git.repo_path)
-	vim.fn.system("git fetch")
-	vim.fn.chdir(current_directory)
-	git.get_current_revision:after(function()
-		git.compare_local_to_remote:start()
-	end)
-	git.get_current_revision:start()
-end
+--
+-- git.get_current_revision = Job:new({
+-- 	command = "git",
+-- 	args = { "rev-parse", "HEAD" },
+-- 	cwd = git.repo_path,
+-- 	on_exit = function(job, return_val)
+-- 		if return_val == 0 then
+-- 			local commit_hash = table.concat(job:result(), "\n")
+-- 			git.current_revision = commit_hash
+-- 		else
+-- 			print("Failed to get the current revision")
+-- 		end
+-- 	end,
+-- })
+--
+-- git.compare_local_to_remote = Job:new({
+-- 	command = "git",
+-- 	args = { "ls-remote", "origin", "HEAD" },
+-- 	cwd = git.repo_path,
+-- 	on_exit = function(job, return_val)
+-- 		if return_val == 0 then
+-- 			local result = table.concat(job:result(), "\n")
+-- 			print(result)
+-- 			local remote_commit_hash = result:match("^(.*)\t") or result
+-- 			print("Local: " .. git.current_revision)
+-- 			print("Remote: " .. remote_commit_hash)
+-- 			if remote_commit_hash == git.current_revision then
+-- 				print("Up to date")
+-- 			else
+-- 				print("Update available")
+-- 			end
+-- 		else
+-- 			print("Failed to get the remote revision")
+-- 		end
+-- 	end,
+-- })
+--
+-- git.check_update_available = function()
+-- 	git.fetch:after(function()
+-- 		git.get_current_revision:start()
+-- 	end)
+-- 	git.get_current_revision:after(function()
+-- 		git.compare_local_to_remote:start()
+-- 	end)
+-- 	git.fetch:start()
+-- end
+--
 
 git.blocking_pull = function()
 	local current_directory
